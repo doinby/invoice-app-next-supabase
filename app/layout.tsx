5d5cd4dc6e0629551f9f249871062bed01b197fe
Kacy Do <contact@kacydo.dev>
2025-08-02 19:24:25 +1000

    add assets and utils functions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import ReactQueryProvider from '@/lib/ReactQueryProvider';
+import ReactQueryProvider from '@/ultils/ReactQueryProvider';
 import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
