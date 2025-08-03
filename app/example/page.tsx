e4b3371244fb7ebf765abc18c21a939becff3195
Kacy Do <contact@kacydo.dev>
2025-08-03 16:08:17 +1000

    update query in  example page

--- app/example/page.tsx
+++ app/example/page.tsx
@@ -1,7 +1,13 @@
-import supabaseClient from '@/lib/supabase-browser';
+import supabaseClient from '@/ultils/supabase-browser';
 
-export default async function Instruments() {
+export default async function Invoices() {
 	const supabase = supabaseClient;
-	const { data: instruments } = await supabase.from('instruments').select();
+	const { data: instruments } = await supabase
+		.from('invoices_v02')
+		.select(
+			`*, customer:customers_v02 (*, customer_addresses_v02 (*)), invoice_items:invoice_line_items_v02 (*, products_v02 (*, product_prices_v02 (*)))`
+		);
+	// .eq('id', 457)
+	// .single();
 	return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
 }
