74cd389a2c4ea066a2e0ae074ea6d56a1b0ec459
Kacy Do <contact@kacydo.dev>
2025-08-27 16:16:26 +1000

    adjust layout and add theme colours

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang='en' data-theme='dim'>
-			<body className='antialiased m-h-full grid grid-cols-[auto_1fr] grid-rows-[1fr_auto]'>
+			<body className='antialiased'>
 				<ReactQueryProvider>
 					<Sidebar />
 					{children}
