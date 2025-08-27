270f5c48e9193bb699f09e20cde33b16b6cccedd
Kacy Do <contact@kacydo.dev>
2025-08-27 10:27:14 +1000

    integrate Supabase, ReactQuery

--- ultils/ReactQueryProvider.tsx
+++ ultils/ReactQueryProvider.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { ReactChildren } from '@/types';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
@@ -8,7 +7,7 @@ import { useState } from 'react';
 export default function ReactQueryProvider({
 	children,
 }: {
-	children: ReactChildren;
+	children: React.ReactNode;
 }) {
 	const [client] = useState(new QueryClient());
 
